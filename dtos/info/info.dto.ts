a9fb6f1d6c5d11a8039f61145bf487bfcc6fb986
Thomas Papamichail <thomas@pointer.gr>
2025-06-23 22:45:28 +0300

    refactor: restructure info DTOs with improved validation and documentation

--- dtos/info/info.dto.ts
+++ dtos/info/info.dto.ts
@@ -7,6 +7,7 @@ import {
   IsNotEmpty,
   MinLength,
   IsEnum,
+  IsObject,
 } from 'class-validator';
 import { Type } from 'class-transformer';
 import { ActionsEnum } from '../../enums/actions.enum';
@@ -17,6 +18,121 @@ import { UnitDto } from '../unit.dto';
 import { LanguageEnum } from '../../enums/language.enum';
 import { RolesEnum } from '../../enums/roles.enum';
 import { ActionDto } from '../action.dto';
+
+/**
+ * DTO for integration information
+ * Contains all information related to a service integration
+ */
+export class InfoDto {
+  /**
+   * The title of the integration
+   */
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  /**
+   * The logo of the integration (optional)
+   */
+  @IsString()
+  @IsOptional()
+  logo?: string;
+
+  /**
+   * Description of the integration and its services (optional)
+   */
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  /**
+   * List of supported languages for the integration.
+   */
+  @IsArray()
+  @IsEnum(LanguageEnum, { each: true })
+  supported_languages: LanguageEnum[];
+
+  /**
+   * Custom attributes for products.
+   */
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => FieldDto)
+  @IsOptional()
+  product_attributes?: FieldDto[];
+
+  /**
+   * Custom attributes for items.
+   */
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => FieldDto)
+  @IsOptional()
+  item_attributes?: FieldDto[];
+
+  /**
+   * Events that the integration listens to.
+   */
+  @IsArray()
+  @IsEnum(EventsEnum, { each: true })
+  @IsOptional()
+  listen_events?: EventsEnum[];
+
+  /**
+   * The roles that need to be accepted by the company
+   */
+  @IsArray()
+  @IsEnum(RolesEnum, { each: true })
+  @IsOptional()
+  requiredRoles?: RolesEnum[];
+
+  /**
+   * Actions that are not supported by the integration.
+   */
+  @IsArray()
+  @IsEnum(ActionsEnum, { each: true })
+  @IsOptional()
+  unsupportedActions?: ActionsEnum[] = [];
+
+  /**
+   * Configuration for the admin panel.
+   */
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => AdminPanelDto)
+  adminPanel?: AdminPanelDto;
+
+  /**
+   * Configuration for the client panel.
+   */
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => ClientPanelDto)
+  clientPanel?: ClientPanelDto;
+
+  /**
+   * The url for the onboarding process after installation of the integration
+   */
+  @IsOptional()
+  @IsUrl()
+  onboardingUrl?: string;
+
+  /**
+   * Units for pay-per-use billing.
+   */
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => UnitDto)
+  @IsOptional()
+  payPerUseUnits?: UnitDto[];
+
+  /**
+   * Mapping of response data field names.
+   */
+  @IsObject()
+  @IsOptional()
+  responseDataFieldNames?: Record<keyof ResponseDataDto, string>;
+}
 
 /**
  * DTO for tabs
@@ -72,86 +188,146 @@ class MenuDto {
 }
 
 /**
- * DTO for integration information
- * Contains all information related to a service integration
+ * DTO for the tabs in the admin panel.
  */
-export class InfoDto {
+class AdminPanelTabsDto {
   /**
-   * The title of the integration
+   * Tabs related to products.
    */
-  @IsString()
-  @IsNotEmpty()
-  title: string;
-
-  /**
-   * The logo of the integration (optional)
-   */
-  @IsString()
-  @IsOptional()
-  logo?: string;
-
-  /**
-   * Description of the integration and its services (optional)
-   */
-  @IsString()
-  @IsOptional()
-  description?: string;
-
-  @IsArray({ each: true })
-  @IsEnum(LanguageEnum)
-  supported_languages: LanguageEnum[];
-
-  product_attributes?: FieldDto[];
-
-  item_attributes?: FieldDto[];
-
-  listen_events?: EventsEnum[];
-
- /**
- * The roles that need to be accepted by the company
- */
   @IsArray()
-  @IsEnum(RolesEnum, { each: true })
-  requiredRoles?: RolesEnum[];
-
-  unsupportedActions: ActionsEnum[] = [];
-  
-  adminPanel?: {
-    productTabs?: TabDto[];
-
-    actions?: {
-      client?: ActionDto[];
-      item?: ActionDto[];
-    };
-
-    menu?: MenuDto;
-
-    /**
-    * Option that will appear in the "Settings" section (optional)
-    */
-    settings?: MenuDto;
-  };
-
-  clientPanel?: {
-    // TODO Ισως να αλλαξουν ορολολογίες οταν σχεδιασουμε το client panel
-    // πχ topMenu και leftMenu
-    productTabs?: TabDto[];
-
-    actions?: {
-      item?: ActionDto[];
-    };
-
-    menu?: MenuDto;
-  };
+  @ValidateNested({ each: true })
+  @Type(() => TabDto)
+  product: TabDto[];
 
   /**
-   * The url for the onboarding process after installation of the integration
+   * Tabs related to items.
+   */
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => TabDto)
+  item: TabDto[];
+
+  /**
+   * Tabs related to clients.
+   */
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => TabDto)
+  client: TabDto[];
+}
+
+/**
+ * DTO for actions available in a panel.
+ */
+class PanelActionsDto {
+  /**
+   * Actions related to clients.
+   */
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => ActionDto)
+  @IsOptional()
+  client?: ActionDto[];
+
+  /**
+   * Actions related to items.
+   */
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => ActionDto)
+  @IsOptional()
+  item?: ActionDto[];
+}
+
+/**
+ * DTO for the admin panel configuration.
+ */
+class AdminPanelDto {
+  /**
+   * Configuration for the tabs in the admin panel.
    */
   @IsOptional()
-  @IsUrl()
-  onboardingUrl?: string;
+  @ValidateNested()
+  @Type(() => AdminPanelTabsDto)
+  tabs?: AdminPanelTabsDto;
 
-  payPerUseUnits?: UnitDto[];
+  /**
+   * Additional actions available in the admin panel.
+   */
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => PanelActionsDto)
+  moreActions?: PanelActionsDto;
 
-  responseDataFieldNames?: Record<keyof ResponseDataDto, string>;
+  /**
+   * Main menu for the admin panel.
+   */
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => MenuDto)
+  menu?: MenuDto;
+
+  /**
+   * Settings menu for the admin panel.
+   */
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => MenuDto)
+  settings?: MenuDto;
+}
+
+/**
+ * DTO for the tabs in the client panel.
+ */
+class ClientPanelTabsDto {
+  /**
+   * Tabs related to items.
+   */
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => TabDto)
+  item: TabDto[];
+}
+
+/**
+ * DTO for actions available in the client panel.
+ */
+class ClientPanelActionsDto {
+  /**
+   * Actions related to items.
+   */
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => ActionDto)
+  @IsOptional()
+  item?: ActionDto[];
+}
+
+/**
+ * DTO for the client panel configuration.
+ */
+class ClientPanelDto {
+  /**
+   * Configuration for the tabs in the client panel.
+   */
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => ClientPanelTabsDto)
+  tabs?: ClientPanelTabsDto;
+
+  /**
+   * Additional actions available in the client panel.
+   */
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => ClientPanelActionsDto)
+  moreActions?: ClientPanelActionsDto;
+
+  /**
+   * Main menu for the client panel.
+   */
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => MenuDto)
+  menu?: MenuDto;
 }
