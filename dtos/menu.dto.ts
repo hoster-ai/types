bc85584bd6699204174fe99cd425c55c117a9d2f
Thomas Papamichail <thomas@pointer.gr>
2025-06-24 20:30:15 +0300

    feat: add type discriminators and prevent overlapping fields in menu DTOs

--- dtos/menu.dto.ts
+++ dtos/menu.dto.ts
@@ -19,18 +19,24 @@ interface BaseMenuDto {
  * Menu item with URL (no submenu)
  */
 export interface MenuDtoWithUrl extends BaseMenuDto {
+  type: 'only-url';
   /**
    * The URL associated with the tab.
    * The requests coming from the hoster will be signed
    * with jwt, which will contain information about the company
    */
   url: string;
+  
+  submenu?: never; // Explicitly prevents submenu
 }
 
 /**
  * Menu item with submenu (no URL)
  */
 export interface MenuDtoWithSubmenu extends BaseMenuDto {
+  type: 'with-submenu';
+  url?: never; // Explicitly prevents url
+  
   /**
    * The list of tabs that will appear in the submenu and as a navigation bar above the main content
    * In case of only one tab, there will be neither a submenu nor a navigation bar.
