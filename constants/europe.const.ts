0a6ff1609df84127c9362102579beeb6d8dd5b16
AthanThom <konialis789@gmail.com>
2025-12-22 09:14:50 +0200

    refactor: improve formatting and add comments to European country constants
    Reformat GEOGRAPHIC_EUROPEAN_COUNTRIES and EU_EEA_COUNTRIES arrays with consistent spacing and line breaks. Add comments explaining EEA countries (Norway, Iceland, Liechtenstein) and Switzerland's special bilateral agreement status.

--- constants/europe.const.ts
+++ constants/europe.const.ts
@@ -3,20 +3,24 @@
  * includes EU members, EEA, microstates, and other European countries
  */
 export const GEOGRAPHIC_EUROPEAN_COUNTRIES = new Set([
-  'AT','BE','BG','HR','CY','CZ','DK','EE','FI','FR','DE','GR',
-  'HU','IE','IT','LV','LT','LU','MT','NL','PL','PT','RO','SK',
-  'SI','ES','SE',
+    'AT', 'BE', 'BG', 'HR', 'CY', 'CZ', 'DK', 'EE', 'FI', 'FR', 'DE', 'GR',
+    'HU', 'IE', 'IT', 'LV', 'LT', 'LU', 'MT', 'NL', 'PL', 'PT', 'RO', 'SK',
+    'SI', 'ES', 'SE',
 
-  'AL','AD','BA','BY','FO','GI','GG','IS','IM','JE','XK','LI',
-  'MD','MC','ME','MK','NO','RU','SM','RS','CH','UA','VA','UK','GB'
+    'AL', 'AD', 'BA', 'BY', 'FO', 'GI', 'GG', 'IS', 'IM', 'JE', 'XK', 'LI',
+    'MD', 'MC', 'ME', 'MK', 'NO', 'RU', 'SM', 'RS', 'CH', 'UA', 'VA', 'UK', 'GB'
 ]);
 
 /**
  * list of EU/EEA member countries 
  */
 export const EU_EEA_COUNTRIES = new Set([
-  'AT','BE','BG','HR','CY','CZ','DK','EE','FI','FR','DE','GR',
-  'HU','IE','IT','LV','LT','LU','MT','NL','PL','PT','RO','SK',
-  'SI','ES','SE',
-  'XI',
+    'AT', 'BE', 'BG', 'HR', 'CY', 'CZ', 'DK', 'EE', 'FI', 'FR', 'DE', 'GR',
+    'HU', 'IE', 'IT', 'LV', 'LT', 'LU', 'MT', 'NL', 'PL', 'PT', 'RO', 'SK',
+    'SI', 'ES', 'SE',
+    'XI',
+    // EEA, EU + Norway, Iceland, Liechtenstein
+    // 'IS', 'LI', 'NO',
+    // special cases
+    //'CH' // Switzerland has bilateral agreements, it is special case
 ]);
